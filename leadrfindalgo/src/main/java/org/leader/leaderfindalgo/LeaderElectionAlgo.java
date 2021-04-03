52d06eb3cc893a412f1c61cce894d6d6c3a1c65b
kamaleshpati <k97pati@gmail.com>
2021-04-03 12:53:10 +0530

    connection to zookeeper

---
+++ leadrfindalgo/src/main/java/org/leader/leaderfindalgo/LeaderElectionAlgo.java
@@ -0,0 +1,49 @@
+package org.leader.leaderfindalgo;
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.io.IOException;
+
+public class LeaderElectionAlgo implements Watcher {
+    private static final String ZOOKEEPER_ADDR = "localhost:2181";
+    private static final int SESSION_TIMEOUT = 3000;
+    private ZooKeeper zooKeeper;
+
+    public void connectZookeeper(){
+        try {
+            this.zooKeeper = new ZooKeeper(ZOOKEEPER_ADDR,SESSION_TIMEOUT,this);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run(){
+        synchronized (zooKeeper){
+            try {
+                zooKeeper.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void process(WatchedEvent watchedEvent) {
+        switch (watchedEvent.getType()){
+            case None:
+                if(watchedEvent.getState() == Event.KeeperState.SyncConnected){
+                    System.out.println("connected to zookeeper");
+                }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        LeaderElectionAlgo leaderElectionAlgo = new LeaderElectionAlgo();
+        leaderElectionAlgo.connectZookeeper();
+        leaderElectionAlgo.run();
+    }
+}
